5c4d68b085ab9ceb6590a5c53c259cb4ef42436a
Daniel Llewellyn <daniel@bowlhat.net>
2017-03-28 01:13:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# sftp-deploy
+
+This is a deployment script for sftp-only sites. It is written in Go. Originally developed with specifics for [Bang Communications](http://www.bang-on.net/), and subsequently generalised and released publicly.
+
+## Installation
+To install you first need to get the Go Compiler from either your OS package repository or the [Go Homepage](https://golang.org/).
+
+Once you've got Go installed you need to set aside a folder for all Go projects to reside within. This folder needs to be declared in an environment variable called GOPATH. (It's advisable to set this somewhere that it is permanent.)
+
+Now that the GOPATH is defined you can carry-on and run `go get github.com/bowlhat/sftp-deploy` which will fetch the code and all dependencies and compile it all into a single binary which will be stored at `$GOPATH/bin/sftp-deploy`.
+
+## Configuration
+Configuration is handled via a YAML file which must be specified using the `-config` commandline flag.
+
+The configuration format is as follows:
+
+```yaml
+connection:
+  username: ""
+  password: ""
+  hostname: "127.0.0.1"
+  port: 22
+backup:
+  to: "local/path/to/backup/folder"
+  from:
+    - "remote/folder/path"
+    - "remote/folder/path2"
+    - ...
+download:
+  - local: "local/destination/folder1"
+    remote: "remote/source/folder1"
+  - local: "local/destination/folder2"
+    remote: "remote/source/folder2"
+  - ...
+upload:
+  - local: "local/source/folder1"
+    remote: "remote/destination/folder1"
+  - local: "local/source/folder2"
+    remote: "remote/destination/folder2"
+  - ...
+```
+
+## Running
+To get a list of available flags, their meanings and default values run `./sftp-deploy -help`.
+
+When deploying to a live site it is recommended to use the `-backup=true` flag, which will create a time-stamped .tar.gz archive of the files that the program removes. This file can be found in the backup folder specified in the configuration file, which will be created if it doesn't already exist. As the backup is time-stamped there is no need to remove an old backup before running the program as it won't get overwritten.
+
+Ideally you will have an ssh-agent running with a valid key loaded which is accepted by the sftp server. (On Windows the supported ssh-agent is Pageant which is available from [the PuTTY project](http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html)) However you can use the configuration file to specify a password if required. The program will NOT prompt for a password interactively so if password-based authentication is required then you MUST supply the password in the configuration.
+
+## Related Code
+All these repositories are included automatically by the `go get` command. They're documented here for completeness.
+
+- Our custom [SSH Agent abstraction](https://github.com/bowlhat/ssh-agent)
+- The spinner is an from [Brian Downs spinner project](https://github.com/briandowns/spinner)
+- [Progressbars](https://github.com/cheggaaa/pb) are powered by cheggaaa's progressbar project 
---
+++ deploy.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"sync/atomic"
+
+	sftpclient "github.com/bowlhat/sftp-client"
+
+	"strings"
+
+	"github.com/cheggaaa/pb"
+	"gopkg.in/yaml.v2"
+)
+
+// YAML Configuration file
+type configuration struct {
+	Connection struct {
+		Username string
+		Password string
+		Hostname string
+		Port     int
+	}
+	Backup struct {
+		To   string
+		From []string
+	}
+	Download []sftpclient.FolderMapping
+	Upload   []sftpclient.FolderMapping
+}
+
+type errorResponse struct {
+	Err error
+}
+
+var (
+	configfilename = flag.String("config", "", "YAML configuration file")
+	doBackup       = flag.Bool("backup", true, "Backup files on remote system")
+	doDownload     = flag.Bool("download", false, "Download files from remote system")
+	doUpload       = flag.Bool("upload", false, "Upload local files to remote system")
+	debugging      = flag.Int("debug", 1, "Spew debugging info, e.g. output every file as it's touched. 0=silent, 1=progress, 2=verbose, 3=firehose")
+
+	remoteFiles []string
+)
+
+func main() {
+	flag.Parse()
+
+	if *configfilename == "" {
+		log.Fatalf("config flag indicating configuration file is required")
+	}
+
+	configfile, err := ioutil.ReadFile(*configfilename)
+	if err != nil {
+		log.Fatalf("could not read config file: %v", err)
+	}
+
+	config := configuration{}
+	if err := yaml.Unmarshal(configfile, &config); err != nil {
+		log.Fatalf("YAML error: %v", err)
+	}
+
+	upload := *doUpload
+	backup := *doBackup
+	download := *doDownload
+
+	// start ssh client on tcp connection
+	sftp, err := sftpclient.New(
+		config.Connection.Hostname,
+		config.Connection.Port,
+		config.Connection.Username,
+		config.Connection.Password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var bar *pb.ProgressBar
+	if *debugging <= 1 {
+		bar = pb.New(0)
+	}
+
+	if backup == true {
+		files, err := sftp.FindAllRemoteFiles(config.Backup.From)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		responses := sftp.BackupFiles(config.Backup.To, files)
+		errorsEncountered := false
+		for response := range responses {
+			errorsEncountered = true
+			log.Println(response.Err)
+		}
+		if errorsEncountered {
+			log.Fatalln("Backup failed. Quitting.")
+		}
+	}
+
+	if download == true {
+		downloadedFile := make(chan bool)
+		downloadDone := make(chan bool)
+		errorsChannel := make(chan errorResponse)
+
+		if *debugging <= 1 {
+			bar.Prefix("Downloading... ")
+			bar.Start()
+		}
+
+		for _, folder := range config.Download {
+			go func(f sftpclient.FolderMapping) {
+				defer func() {
+					downloadDone <- true
+				}()
+
+				media, err := sftp.FindAllRemoteFiles([]string{f.Remote})
+				if err != nil {
+					errorsChannel <- errorResponse{err}
+					return
+				}
+				atomic.AddInt64(&bar.Total, int64(len(media)))
+				for _, remotefile := range media {
+					trimmed := strings.TrimPrefix(remotefile, f.Remote)
+					localfile := strings.Join([]string{f.Local, trimmed}, "/")
+					if err := sftp.GetFile(localfile, remotefile); err != nil {
+						errorsChannel <- errorResponse{err}
+					}
+					downloadedFile <- true
+				}
+			}(folder)
+		}
+
+		go func() {
+			for range downloadedFile {
+				<-downloadedFile
+				bar.Increment()
+			}
+		}()
+
+		go func() {
+			for err := range errorsChannel {
+				log.Println(err)
+			}
+		}()
+
+		for range config.Download {
+			<-downloadDone
+		}
+		close(downloadDone)
+		close(downloadedFile)
+		close(errorsChannel)
+		if *debugging <= 1 {
+			bar.FinishPrint("Download complete.")
+		}
+	}
+
+	if upload == true {
+		if *debugging <= 1 {
+			bar.Prefix("Uploading... ")
+			bar.Start()
+		}
+
+		errorChannel, countChannel, copiedCountChannel := sftp.Upload(config.Upload)
+
+		go func() {
+			for range countChannel {
+				atomic.AddInt64(&bar.Total, 1)
+			}
+		}()
+		go func() {
+			for range copiedCountChannel {
+				bar.Increment()
+			}
+		}()
+
+		for response := range errorChannel {
+			log.Println(response.Err)
+		}
+	}
+}
